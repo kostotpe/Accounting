4d532ac5ba5a68a753faea7a3f809720d7b0a169
kostotpe <a19910304@hotmail.com>
2023-03-25 18:51:42 +0800

    v2

--- accounting.py
+++ accounting.py
@@ -9,8 +9,9 @@ while True:
     p = []
     # p = [name, price] 比較快的寫法
     p.append(name)
-    p.append(price)
+    p.append(price) # 存取二為清單 products[0][0]
     products.append(p) # 更快寫法 products.append([name, price]) 省略上面三行
 print(products)
 
-products[0][0] # 存取二為清單
+for p in products:
+    print(p[0], '的價格是', p[1])
