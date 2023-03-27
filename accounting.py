6b9866b1f1945ce30981e01a1d64dca4923fb9f6
kostotpe <a19910304@hotmail.com>
2023-03-27 11:17:06 +0800

    v5

--- accounting.py
+++ accounting.py
@@ -4,13 +4,26 @@
 # 加入欄位名稱
 # 加入可讀取檔案及split切割功能
 # continue 跳到下一迴的意思，但必須在迴圈內才能作用，不會跳出迴圈
+# 檢查檔案在不在
+import os #operating system
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '品名,價格' in line:
-            continue # 繼續，不會跳出迴圈
-        name, price = line.strip().split(',') # split 切割的意思,分行之類的,如例子遇','換行
-        products.append([name, price])
+#檢查檔案室否在電腦裡
+if os.path.isfile('products.csv'):
+    print('有')
+    #讀取檔案
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '品名,價格' in line:
+                continue # 繼續，不會跳出迴圈
+            name, price = line.strip().split(',') # split 切割的意思,分行之類的,如例子遇','換行
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案')
+
+
+# 讓使用者輸入     
 while True:
     name = input('輸入品名: ')
     if name == 'q':
@@ -23,11 +36,13 @@ while True:
     products.append(p) # 更快寫法 products.append([name, price]) 省略上面三行
 print(products)
 
+# 印出所有購買紀錄
 for p in products:
     print(p[0], '的價格是', p[1])
 
 # 字串可以用+跟* 如 'ab' + 'ab'= 'abab' 'ab' * 3 = 'ababab'
 
+#寫入檔案
 with open ('products.csv', 'w', encoding='utf-8') as f: # 文字讀取編碼要在這加入
     f.write('品名,價格\n' )
     for p in products:
