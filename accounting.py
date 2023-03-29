ef4af19cd9429d2f422305f7245975bcf98e0197
kostotpe <a19910304@hotmail.com>
2023-03-29 13:58:06 +0800

    v6

--- accounting.py
+++ accounting.py
@@ -7,54 +7,63 @@
 # 檢查檔案在不在
 import os #operating system
 
-products = []
-#檢查檔案室否在電腦裡
-if os.path.isfile('products.csv'):
-    print('有')
-    #讀取檔案
-    with open('products.csv', 'r', encoding='utf-8') as f:
-        for line in f:
-            if '品名,價格' in line:
-                continue # 繼續，不會跳出迴圈
-            name, price = line.strip().split(',') # split 切割的意思,分行之類的,如例子遇','換行
-            products.append([name, price])
+#讀取檔案
+def read_file(filename):
+    products = []
+    if os.path.isfile(filename): #檢查檔案室否在電腦裡
+        print('有')
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                if '品名,價格' in line:
+                    continue # 繼續，不會跳出迴圈
+                name, price = line.strip().split(',') # split 切割的意思,分行之類的,如例子遇','換行
+                products.append([name, price])
+        print(products)
+    else:
+        print('找不到檔案')
+    return products
+
+# 讓使用者輸入
+def user_input(products):     
+    while True:
+        name = input('輸入品名: ')
+        if name == 'q':
+            break
+        price = input('輸入價格$: ')
+        p = []
+        # p = [name, price] 比較快的寫法
+        p.append(name)
+        p.append(price) # 存取二為清單 products[0][0]
+        products.append(p) # 更快寫法 products.append([name, price]) 省略上面三行
     print(products)
-else:
-    print('找不到檔案')
-
-
-# 讓使用者輸入     
-while True:
-    name = input('輸入品名: ')
-    if name == 'q':
-        break
-    price = input('輸入價格$: ')
-    p = []
-    # p = [name, price] 比較快的寫法
-    p.append(name)
-    p.append(price) # 存取二為清單 products[0][0]
-    products.append(p) # 更快寫法 products.append([name, price]) 省略上面三行
-print(products)
+    return products
 
 # 印出所有購買紀錄
-for p in products:
-    print(p[0], '的價格是', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
 
 # 字串可以用+跟* 如 'ab' + 'ab'= 'abab' 'ab' * 3 = 'ababab'
 
 #寫入檔案
-with open ('products.csv', 'w', encoding='utf-8') as f: # 文字讀取編碼要在這加入
-    f.write('品名,價格\n' )
-    for p in products:
-        f.write(p[0] + ',' + p[1] + '\n') # 有','excel才會不同格 \n生成後會自動換行
+def write_file(filename, products):
+    with open (filename, 'w', encoding='utf-8') as f: # 文字讀取編碼要在這加入
+        f.write('品名,價格\n' )
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n') # 有','excel才會不同格 \n生成後會自動換行
 
-# 練習寫入檔案
-data = [1, 3, 5, 7, 9]
-with open('test.txt', 'w') as test:
-    for c in data:
-        test.write(str(c) + '\n')
-# 解答寫法
+products = read_file('products.csv')
+products = user_input(products)
+print_products(products)
+write_file('products.csv', products)
+
+# # 練習寫入檔案
 # data = [1, 3, 5, 7, 9]
-# with open('file.txt', 'w') as f:
-    # for d in data:
-        # f.write(str(d) + '\n')
+# with open('test.txt', 'w') as test:
+#     for c in data:
+#         test.write(str(c) + '\n')
+# # 解答寫法
+# # data = [1, 3, 5, 7, 9]
+# # with open('file.txt', 'w') as f:
+#     # for d in data:
+#         # f.write(str(d) + '\n')
